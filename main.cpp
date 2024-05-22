a997e4f30021a0a048aa8ab3d7f943d67e0602c8
đức hoan <160832222+DucHoan23020607@users.noreply.github.com>
2024-05-23 01:37:58 +0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -205,206 +205,232 @@ void FlappyBird::close() {
 
 void FlappyBird::render() {
     SDL_RenderClear(gRenderer);
-
     SDL_RenderCopy(gRenderer, gBackgroundTexture, nullptr, nullptr);
 
-    SDL_RenderCopy(gRenderer, gBirdTexture, nullptr, &gBirdRect);
+   SDL_RenderCopy(gRenderer, gBirdTexture, nullptr, &gBirdRect);
 
-    for (const auto& pipePair : pipes) {
-        SDL_RenderCopy(gRenderer, gPipeUpTexture, nullptr, &pipePair.upperRect);
-        SDL_RenderCopy(gRenderer, gPipeDownTexture, nullptr, &pipePair.lowerRect);
-    }
+   for (const auto& pipePair : pipes) {
+       SDL_RenderCopy(gRenderer, gPipeUpTexture, nullptr, &pipePair.upperRect);
+       SDL_RenderCopy(gRenderer, gPipeDownTexture, nullptr, &pipePair.lowerRect);
+   }
 
-    if (gameState == GAME_OVER) {
-        showGameOverScreen();
-        renderGameOverText();
-    } else if (gameState == PAUSED) {
-        showPauseScreen();
-    }
+   if (gameState == GAME_OVER) {
+       showGameOverScreen();
+       renderGameOverText();
+   } else if (gameState == PAUSED) {
+       showPauseScreen();
+   }
 
-    renderScore();
+   renderScore();
 
-    SDL_RenderPresent(gRenderer);
+   SDL_RenderPresent(gRenderer);
 }
 
 void FlappyBird::renderGameOverText() {
-    SDL_Color redColor = {255, 0, 0, 255};
-    TTF_Font* gameOverFont = TTF_OpenFont("font/Font.ttf", 50);
-    if (gameOverFont == nullptr) {
-        std::cerr << "Failed to load font for game over text: " << TTF_GetError() << std::endl;
-        return;
-    }
-    SDL_Surface* gameOverSurface = TTF_RenderText_Solid(gameOverFont, "Game Over", redColor);
-    if (gameOverSurface == nullptr) {
-        std::cerr << "Failed to create surface for game over text: " << TTF_GetError() << std::endl;
-        TTF_CloseFont(gameOverFont);
-        return;
-    }
-    SDL_Texture* gameOverTexture = SDL_CreateTextureFromSurface(gRenderer, gameOverSurface);
-    if (gameOverTexture == nullptr) {
-        std::cerr << "Failed to create texture for game over text: " << SDL_GetError() << std::endl;
-        SDL_FreeSurface(gameOverSurface);
-        TTF_CloseFont(gameOverFont);
-        return;
-    }
-    SDL_Rect gameOverRect = {320 - (gameOverSurface->w / 2), 100, gameOverSurface->w, gameOverSurface->h};
-    SDL_RenderCopy(gRenderer, gameOverTexture, nullptr, &gameOverRect);
-    SDL_FreeSurface(gameOverSurface);
-    SDL_DestroyTexture(gameOverTexture);
-    TTF_CloseFont(gameOverFont);
+   SDL_Color redColor = {255, 0, 0, 255};
+   TTF_Font* gameOverFont = TTF_OpenFont("font/Font.ttf", 50);
+   if (gameOverFont == nullptr) {
+       std::cerr << "Failed to load font for game over text: " << TTF_GetError() << std::endl;
+       return;
+   }
+   SDL_Surface* gameOverSurface = TTF_RenderText_Solid(gameOverFont, "Game Over", redColor);
+   if (gameOverSurface == nullptr) {
+       std::cerr << "Failed to create surface for game over text: " << TTF_GetError() << std::endl;
+       TTF_CloseFont(gameOverFont);
+       return;
+   }
+   SDL_Texture* gameOverTexture = SDL_CreateTextureFromSurface(gRenderer, gameOverSurface);
+   if (gameOverTexture == nullptr) {
+       std::cerr << "Failed to create texture for game over text: " << SDL_GetError() << std::endl;
+       SDL_FreeSurface(gameOverSurface);
+       TTF_CloseFont(gameOverFont);
+       return;
+   }
+   SDL_Rect gameOverRect = {320 - (gameOverSurface->w / 2), 100, gameOverSurface->w, gameOverSurface->h};
+   SDL_RenderCopy(gRenderer, gameOverTexture, nullptr, &gameOverRect);
+   SDL_FreeSurface(gameOverSurface);
+   SDL_DestroyTexture(gameOverTexture);
+   TTF_CloseFont(gameOverFont);
 }
 
 void FlappyBird::showPauseScreen() {
-    SDL_Color whiteColor = { 255, 255, 255, 255 };
-    SDL_Surface* pauseSurface = TTF_RenderText_Solid(gFont, "Press SPACE to Play", whiteColor);
-    SDL_Texture* pauseTexture = SDL_CreateTextureFromSurface(gRenderer, pauseSurface);
+   SDL_Color whiteColor = { 255, 255, 255, 255 };
+   SDL_Surface* pauseSurface = TTF_RenderText_Solid(gFont, "Press SPACE to Play", whiteColor);
+   SDL_Texture* pauseTexture = SDL_CreateTextureFromSurface(gRenderer, pauseSurface);
 
-    SDL_Rect pauseRect;
-    pauseRect.x = 320 - (pauseSurface->w / 2);
-    pauseRect.y = 240 - (pauseSurface->h / 2);
-    pauseRect.w = pauseSurface->w;
-    pauseRect.h = pauseSurface->h;
+   SDL_Rect pauseRect;
+   pauseRect.x = 320 - (pauseSurface->w / 2);
+   pauseRect.y = 240;
+   pauseRect.w = pauseSurface->w;
+   pauseRect.h = pauseSurface->h;
 
-    SDL_RenderCopy(gRenderer, pauseTexture, nullptr, &pauseRect);
-    SDL_FreeSurface(pauseSurface);
-    SDL_DestroyTexture(pauseTexture);
+   SDL_RenderCopy(gRenderer, pauseTexture, nullptr, &pauseRect);
+
+   SDL_FreeSurface(pauseSurface);
+   SDL_DestroyTexture(pauseTexture);
 }
 
 void FlappyBird::showGameOverScreen() {
-    SDL_Color whiteColor = { 255, 255, 255, 255 };
-    SDL_Surface* playAgainSurface = TTF_RenderText_Solid(gFont, "Play Again", whiteColor);
-    playAgainTexture = SDL_CreateTextureFromSurface(gRenderer, playAgainSurface);
-    playAgainRect = { 320 - (playAgainSurface->w / 2), 300, playAgainSurface->w, playAgainSurface->h };
-    SDL_FreeSurface(playAgainSurface);
+   SDL_Color redColor = {255, 0, 0, 255};
 
-    SDL_Surface* quitSurface = TTF_RenderText_Solid(gFont, "Quit", whiteColor);
-    quitTexture = SDL_CreateTextureFromSurface(gRenderer, quitSurface);
-    quitRect = { 320 - (quitSurface->w / 2), 350, quitSurface->w, quitSurface->h };
-    SDL_FreeSurface(quitSurface);
+   SDL_Surface* playAgainSurface = TTF_RenderText_Solid(gFont, "Play Again", redColor);
+   playAgainTexture = SDL_CreateTextureFromSurface(gRenderer, playAgainSurface);
+   SDL_FreeSurface(playAgainSurface);
 
-    SDL_RenderCopy(gRenderer, playAgainTexture, nullptr, &playAgainRect);
-    SDL_RenderCopy(gRenderer, quitTexture, nullptr, &quitRect);
-}
+   SDL_Surface* exitSurface = TTF_RenderText_Solid(gFont, "Exit", redColor);
+   exitTexture = SDL_CreateTextureFromSurface(gRenderer, exitSurface);
+   SDL_FreeSurface(exitSurface);
 
-void FlappyBird::renderScore() {
-    SDL_Color whiteColor = { 255, 255, 255, 255 };
-    std::string scoreText = "Score: " + std::to_string(score);
-    SDL_Surface* scoreSurface = TTF_RenderText_Solid(gFont, scoreText.c_str(), whiteColor);
-    SDL_Texture* scoreTexture = SDL_CreateTextureFromSurface(gRenderer, scoreSurface);
+   playAgainRect.x = 320 - 100;
+   playAgainRect.y = 240;
+   playAgainRect.w = 200;
+   playAgainRect.h = 50;
 
-    SDL_Rect scoreRect;
-    scoreRect.x = 10;
-    scoreRect.y = 10;
-    scoreRect.w = scoreSurface->w;
-    scoreRect.h = scoreSurface->h;
+   exitRect.x = 320 - 100;
+   exitRect.y = 300;
+   exitRect.w = 200;
+   exitRect.h = 50;
 
-    SDL_RenderCopy(gRenderer, scoreTexture, nullptr, &scoreRect);
-    SDL_FreeSurface(scoreSurface);
-    SDL_DestroyTexture(scoreTexture);
+   SDL_RenderCopy(gRenderer, playAgainTexture, nullptr, &playAgainRect);
+   SDL_RenderCopy(gRenderer, exitTexture, nullptr, &exitRect);
 }
 
 void FlappyBird::handleEvents() {
-    SDL_Event e;
-    while (SDL_PollEvent(&e) != 0) {
-        if (e.type == SDL_QUIT) {
-            quit = true;
-        } else if (e.type == SDL_KEYDOWN) {
-            if (e.key.keysym.sym == SDLK_SPACE) {
-                if (gameState == PAUSED) {
-                    gameState = RUNNING;
-                } else if (gameState == RUNNING) {
-                    gBirdRect.y -= 50;
-                } else if (gameState == GAME_OVER) {
-                    gameState = PAUSED;
-                    gBirdRect.y = 200;
-                    pipes.clear();
-                    generatePipePair();
-                    score = 0;
-                }
-            }
-        } else if (e.type == SDL_MOUSEBUTTONDOWN) {
-            int mouseX = e.button.x;
-            int mouseY = e.button.y;
-            if (gameState == GAME_OVER) {
-                if (mouseX >= playAgainRect.x && mouseX <= playAgainRect.x + playAgainRect.w && mouseY >= playAgainRect.y && mouseY <= playAgainRect.y + playAgainRect.h) {
-                    gameState = PAUSED;
-                    gBirdRect.y = 200;
-                    pipes.clear();
-                    generatePipePair();
-                    score = 0;
-                } else if (mouseX >= quitRect.x && mouseX <= quitRect.x + quitRect.w && mouseY >= quitRect.y && mouseY <= quitRect.y + quitRect.h) {
-                    quit = true;
-                }
-            }
-        }
-    }
+   SDL_Event e;
+   while (SDL_PollEvent(&e)) {
+       if (e.type == SDL_QUIT) {
+           quit = true;
+       } else if (e.type == SDL_KEYDOWN) {
+           if (e.key.keysym.sym == SDLK_SPACE) {
+               if (gameState == PAUSED) {
+                   gameState = RUNNING;
+               } else if (gameState == RUNNING) {
+                   gBirdRect.y -= 50;
+               }
+           } else if (e.key.keysym.sym == SDLK_RETURN) {
+               if (gameState == RUNNING) {
+                   gameState = PAUSED;
+               }
+           }
+       } else if (e.type == SDL_MOUSEBUTTONDOWN) {
+           int x, y;
+           SDL_GetMouseState(&x, &y);
+           if (gameState == GAME_OVER) {
+               if (x >= playAgainRect.x && x <= playAgainRect.x + playAgainRect.w && y >= playAgainRect.y && y <= playAgainRect.y + playAgainRect.h) {
+                   gameState = RUNNING;
+                   gBirdRect.y = 200;
+                   pipes.clear();
+                   generatePipePair();
+                   score = 0;
+               } else if (x >= exitRect.x && x <= exitRect.x + exitRect.w && y >= exitRect.y && y <= exitRect.y + exitRect.h) {
+                   quit = true;
+               }
+           }
+       }
+   }
 }
 
 void FlappyBird::update() {
-    if (gameState == RUNNING) {
-        gBirdRect.y += gravity;
-        for (auto& pipePair : pipes) {
-            pipePair.upperRect.x -= 5;
-            pipePair.lowerRect.x -= 5;
+   if (gameState == RUNNING) {
+       if (pipes.empty() || pipes.back().upperRect.x < 300) {
+           generatePipePair();
+       }
 
-            if (!pipePair.passed && pipePair.upperRect.x < gBirdRect.x) {
-                pipePair.passed = true;
-                score++;
-            }
+       for (auto& pipePair : pipes) {
+           pipePair.upperRect.x -= 5;
+           pipePair.lowerRect.x -= 5;
 
-            if (pipePair.upperRect.x + pipePair.upperRect.w < 0) {
-                pipes.erase(pipes.begin());
-                generatePipePair();
-            }
-        }
+           if (checkCollision(pipePair.upperRect) || checkCollision(pipePair.lowerRect)) {
+               gameState = GAME_OVER;
+               if (score > highScore) {
+                   highScore = score;
+               }
+               return;
+           }
 
-        if (checkCollision(gBirdRect)) {
-            gameState = GAME_OVER;
-            if (score > highScore) {
-                highScore = score;
-            }
-        }
-    }
+           if (pipePair.upperRect.x + pipePair.upperRect.w < gBirdRect.x && !pipePair.passed) {
+               pipePair.passed = true;
+               updateScore();
+           }
+       }
+
+       pipes.erase(std::remove_if(pipes.begin(), pipes.end(), [](const PipePair& pipePair) { return pipePair.upperRect.x + pipePair.upperRect.w < 0; }), pipes.end());
+
+       gBirdRect.y += gravity;
+
+       if (gBirdRect.y + gBirdRect.h > 480 || gBirdRect.y < 0) {
+           gameState = GAME_OVER;
+           if (score > highScore) {
+               highScore = score;
+           }
+       }
+   }
 }
 
 bool FlappyBird::generatePipePair() {
-    PipePair newPair;
-    newPair.upperRect = { 640, 0, 50, 200 };
-    newPair.lowerRect = { 640, 300, 50, 200 };
-    newPair.passed = false;
-    pipes.push_back(newPair);
-    return true;
+   int gapHeight = 150;
+   int pipeWidth = 70;
+   int minHeight = 50;
+   int maxHeight = 480 - gapHeight - minHeight;
+
+   int upperPipeHeight = minHeight + rand() % (maxHeight - minHeight + 1);
+   int lowerPipeHeight = 480 - upperPipeHeight - gapHeight;
+
+   PipePair pipePair = {{640, 0, pipeWidth, upperPipeHeight}, {640, 480 - lowerPipeHeight, pipeWidth, lowerPipeHeight}, false};
+
+   pipes.push_back(pipePair);
+
+   return true;
 }
 
 bool FlappyBird::checkCollision(const SDL_Rect& rect) {
-    for (const auto& pipePair : pipes) {
-        if (SDL_HasIntersection(&rect, &pipePair.upperRect) || SDL_HasIntersection(&rect, &pipePair.lowerRect)) {
-            return true;
-        }
-    }
+   return SDL_HasIntersection(&gBirdRect, &rect);
+}
 
-    if (rect.y <= 0 || rect.y + rect.h >= 480) {
-        return true;
-    }
+void FlappyBird::updateScore() {
+   score++;
+}
 
-    return false;
+void FlappyBird::renderScore() {
+   std::string scoreText = "Score: " + std::to_string(score);
+   std::string highScoreText = "High Score: " + std::to_string(highScore);
+
+   SDL_Color whiteColor = {255, 255, 255, 255};
+   SDL_Surface* scoreSurface = TTF_RenderText_Solid(gFont, scoreText.c_str(), whiteColor);
+   SDL_Surface* highScoreSurface = TTF_RenderText_Solid(gFont, highScoreText.c_str(), whiteColor);
+
+   SDL_Texture* scoreTexture = SDL_CreateTextureFromSurface(gRenderer, scoreSurface);
+   SDL_Texture* highScoreTexture = SDL_CreateTextureFromSurface(gRenderer, highScoreSurface);
+
+   SDL_Rect scoreRect = {10, 10, scoreSurface->w, scoreSurface->h};
+   SDL_Rect highScoreRect = {10, 40, highScoreSurface->w, highScoreSurface->h};
+
+   SDL_RenderCopy(gRenderer, scoreTexture, nullptr, &scoreRect);
+   SDL_RenderCopy(gRenderer, highScoreTexture, nullptr, &highScoreRect);
+
+   SDL_FreeSurface(scoreSurface);
+   SDL_FreeSurface(highScoreSurface);
+   SDL_DestroyTexture(scoreTexture);
+   SDL_DestroyTexture(highScoreTexture);
 }
 
 void FlappyBird::run() {
-    while (!quit) {
-        handleEvents();
-        update();
-        render();
-        SDL_Delay(16);
-    }
+   while (!quit) {
+       handleEvents();
+       update();
+       render();
+       SDL_Delay(2);
+   }
 }
 
 int main(int argc, char* args[]) {
-    FlappyBird game;
-    if (!game.init()) {
-        std::cerr << "Failed to initialize!" << std::endl;
-        return -1;
-    }
-    game.run();
-    return 0;
+   FlappyBird game;
+
+   if (game.init()) {
+       game.run();
+   } else {
+       std::cerr << "Failed to initialize the game." << std::endl;
+   }
+
+   return 0;
 }
